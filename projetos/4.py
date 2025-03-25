144e4de7c67d2421b37c4035195959145e4eb752
PHELIPE MARQUES LOBATO ANDRADE DA SILVA <00001117661143sp@aluno.educacao.sp.gov.br>
2025-03-25 18:07:07 -0300

    first commit

---
+++ projetos/4.py
@@ -0,0 +1,14 @@
+Nome = input('Digite seu nome de usuario: ')
+Senha = int(input('Digite sua senha: '))
+
+if Nome == 'admin':
+    ('Usuario correto')
+else:
+    ('Usuário ou senha incorretos')
+
+Senha = int(input('Digite sua senha: '))
+
+if Senha == '1234':
+        print('Login bem-sucedido')
+else:
+        ('Usuário ou senha incorretos')
